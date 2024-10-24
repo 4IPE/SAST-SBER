fee47873ca7d9de44e030bbe2a5a8abb7de7b060
4IPE <bykov220033@gmail.com>
2024-10-24 15:45:07 +0300

    <feat> init

---
+++ sast-client/src/test/java/ru/Sber/sast_client/SastClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.Sber.sast_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SastClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ sast-sec/src/test/java/ru/SberTex/sast_sec/SastSecApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.SberTex.sast_sec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SastSecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
