1b8aa5e7b200d67c96137ca1f931616b24575937
4IPE <bykov220033@gmail.com>
2024-10-26 13:20:54 +0300

    <feat> добавление первой схемы бд

---
+++ sast-manager/src/main/resources/schema.sql
@@ -0,0 +1,42 @@
+CREATE TABLE IF NOT EXISTS users (
+    id BIGSERIAL PRIMARY KEY,
+    username VARCHAR NOT NULL UNIQUE,
+    password VARCHAR NOT NULL,
+    );
+
+CREATE TABLE IF NOT EXISTS roles (
+        id BIGSERIAL PRIMARY KEY,
+        role VARCHAR NOT NULL,
+        );
+
+CREATE TABLE IF NOT EXISTS roles_users (
+        user_id BIGINT NOT NULL UNIQUE,
+        role_id BIGINT NOT NULL UNIQUE,
+        FOREIGN KEY(role_id) REFERENCES roles(id),
+        FOREIGN KEY(user_id) REFERENCES users(id),
+        PRIMARY KEY (user_id,role_id)
+        );
+
+
+CREATE TABLE IF NOT EXISTS projects (
+        id BIGSERIAL PRIMARY KEY,
+        name VARCHAR NOT NULL,
+        url VARCHAR NOT NULL,
+        );
+
+CREATE TABLE IF NOT EXISTS reports (
+        id BIGSERIAL PRIMARY KEY,
+        file BYTEA NOT NULL UNIQUE,
+        data TIMESTAMP
+        project_id BIGINT NOT NULL UNIQUE,
+        FOREIGN KEY(project_id) REFERENCES projects(id)
+        );
+
+
+CREATE TABLE IF NOT EXISTS projects_users (
+        project_id BIGINT NOT NULL UNIQUE,
+        user_id BIGINT NOT NULL UNIQUE,
+        FOREIGN KEY(project_id) REFERENCES projects(id),
+        FOREIGN KEY(user_id) REFERENCES users(id),
+        PRIMARY KEY (project_id,reports_id)
+        );
