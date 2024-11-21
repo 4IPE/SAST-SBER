a58a31fe23e1f104622e01f6e678d1b25c76d720
Daniil <99344227+4IPE@users.noreply.github.com>
2024-11-21 19:20:52 +0300

    Revert "Создание связи между агентом и манагером "

--- sast-manager/src/main/java/ru/SberTex/SastManager/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.SberTex.SastManager.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.SberTex.SastManager.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
