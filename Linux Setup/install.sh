30eac23628d979cb78b0f635abe9018cd573bda9
Daniil <99344227+4IPE@users.noreply.github.com>
2025-02-02 21:25:36 +0300

    <fix>Исправление dockerfile и доработка скрипта для запуска

--- Linux Setup/install.sh
+++ Linux Setup/install.sh
@@ -2,40 +2,49 @@
 
 echo "Начинаем установку..."
 
-
 if [ "$(id -u)" -ne 0 ]; then
-    echo "⚠️  Пожалуйста, запустите скрипт с правами root: sudo ./install.sh"
+    echo " Пожалуйста, запустите скрипт с правами root: sudo ./install.sh"
     exit 1
 fi
-
 
 CURRENT_USER=$(logname)
 echo "Добавляем пользователя $CURRENT_USER в группу docker..."
 usermod -aG docker $CURRENT_USER
-
-
-echo "Обновляем пакеты"
+и
+echo "Обновляем пакеты..."
 apt update -y && apt upgrade -y
 
-echo "Устанавливаем Docker и Docker Compose"
-apt install -y docker.io git curl
+echo "Устанавливаем Docker, Docker Compose и Maven"
+apt install -y docker.io git curl maven
 
 if ! [ -x "$(command -v docker-compose)" ]; then
-    echo "Загружаем Docker Compose"
+    echo "Загружаем Docker Compose..."
     curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
     ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 fi
 
-echo "Проверяем версии"
+echo " Проверяем версии..."
 docker --version
 docker-compose --version
+mvn -version
 
+echo "Удаляем старый код (если есть)..."
+rm -rf /opt/SAST-SBER
 
-echo "Клонируем репозиторий."
+echo "Клонируем репозиторий (ветка dev)..."
 cd /opt
 git clone -b dev --single-branch https://github.com/4IPE/SAST-SBER.git
 cd SAST-SBER
 
-echo "Запускаем контейнеры"
-docker-compose up
+echo "Сборка проекта с помощью Maven..."
+mvn clean install -DskipTests
+
+if [ $? -ne 0 ]; then
+    echo "❌ Ошибка сборки проекта! Проверьте логи и исправьте ошибки."
+    exit 1
+fi
+
+echo "Запускаем контейнеры..."
+docker-compose up  --build
+
